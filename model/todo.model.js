f919a6511a5730f88db20d4ed8ee90569cc1f44a
joser3047 <joser3047@gmail.com>
2020-12-07 18:46:24 -0600

    test

--- model/todo.model.js
+++ model/todo.model.js
@@ -11,6 +11,6 @@ const todoSchema = new mongoose.Schema({
     }
 });
 
-const todoModel = mongoose.model('s', todoSchema);
+const todoModel = mongoose.model('todo', todoSchema);
 
 module.exports = todoModel;
